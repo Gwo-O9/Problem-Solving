9fbf6db5d647361f5136d12c7716431bb7379359
GeunuKim <101039305+foolishvoy@users.noreply.github.com>
2022-11-13 00:00:39 +0900

    Update 69. Sqrt(x).java

--- Round2/69. Sqrt(x).java
+++ Round2/69. Sqrt(x).java
@@ -1,5 +1,23 @@
 class Solution {
     public int mySqrt(int x) {
-        return (int) Math.sqrt(x);
+        long low = 0;
+        long high = x;
+        long ans = 0;
+        
+        while(low<= high){
+            long mid = (low+high)/2;
+            
+            if(mid*mid < x){
+                low = mid+1;å
+                ans = mid;
+            } 
+            else if(mid*mid > x){
+                high = mid-1;
+            }
+            else{
+                return (int) mid;
+            }
+        }
+        return (int) ans;
     }
 }
