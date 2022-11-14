4bb969c7e2859b2b2f0254f1f8a9351363e5c8dd
GeunuKim <101039305+foolishvoy@users.noreply.github.com>
2022-11-14 16:35:02 +0900

    Update 35. Search Insert Position.java

--- LeetCode/Round2/35. Search Insert Position.java
+++ LeetCode/Round2/35. Search Insert Position.java
@@ -1,20 +1,30 @@
 class Solution {
     public int searchInsert(int[] nums, int target) {
-        if(target > nums[nums.length-1]){
-            return nums.length;
-        }
+        int left = 0;
+        int right = nums.length-1;
+        int mid = 0;
         
-        for(int i = 0; i<nums.length;i++){
-            if(target > nums[i]){
-                continue;
+        while(left <= right){
+            
+            if(target<nums[left]){
+                return left;
             }
-            else if(target == nums[i]){
-                return i;
+            if(target>nums[right]){
+                return right+1;
             }
-            else if(target < nums[i]){
-                return i;
+            
+            mid = (right+left)/2;
+            
+            if(target < nums[mid]){
+                right = mid-1;
+            }
+            else if(target > nums[mid]){
+                left = mid+1;
+            }
+            else if(target == nums[mid]){
+                return mid;
             }
         }
-        return 0;
+        return -1;
     }
 }
