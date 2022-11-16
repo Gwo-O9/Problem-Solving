28f3204c6a1caeae1038845f6b97ae4d0f014add
GeunuKim <101039305+foolishvoy@users.noreply.github.com>
2022-11-16 15:00:24 +0900

    Update and rename 100. Same Tree to 100. Same Tree.java

--- LeetCode/Round3/100. Same Tree.java
+++ LeetCode/Round3/100. Same Tree.java
@@ -15,6 +15,10 @@
  */
 class Solution {
     public boolean isSameTree(TreeNode p, TreeNode q) {
+        return isSame(p,q);
+    }
+    
+    public boolean isSame(TreeNode p, TreeNode q){
         if(p == null && q == null) return true;
         if(p == null || q == null) return false;
         if(p.val != q.val) return false;
