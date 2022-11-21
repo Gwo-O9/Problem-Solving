8148e7e2c51f067fdf314af5930e4e3e82a324d3
GeunuKim <101039305+foolishvoy@users.noreply.github.com>
2022-11-21 22:41:33 +0900

    Update 160. Intersection of Two Linked Lists.java

--- LeetCode/Round4/160. Intersection of Two Linked Lists.java
+++ LeetCode/Round4/160. Intersection of Two Linked Lists.java
@@ -9,6 +9,9 @@
  *     }
  * }
  */
+
+/* Solution 1*/
+
 public class Solution {
     public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
         ListNode curA = headA;
@@ -21,6 +24,26 @@ public class Solution {
                 curB = curB.next;
             }
             curA=curA.next;
+        }
+        return null;
+    }
+}
+
+/* Solution 2 */
+public class Solution {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        ListNode curA = headA;
+        ListNode curB = headB;
+        HashSet<ListNode> map = new HashSet<>();
+        if(curA == null || curB == null) return null;
+        
+        for(;curB != null;curB = curB.next){
+            map.add(curB);
+        }
+        for(;curA != null;curA = curA.next){
+            if(!map.add(curA)){
+                return curA;
+            }
         }
         return null;
     }
