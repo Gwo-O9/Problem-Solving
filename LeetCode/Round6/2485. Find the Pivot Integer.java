f8a889a1f83f0cbfa857a99d75af936ea426391f
GeunuKim <101039305+foolishvoy@users.noreply.github.com>
2022-11-29 13:37:32 +0900

    Create 2485. Find the Pivot Integer.java

---
+++ LeetCode/Round6/2485. Find the Pivot Integer.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int pivotInteger(int n) {
+        if(n == 1) return 1;
+        for(int i = 1; i<n;i++){
+            int sum1 = 0;
+            int sum2 = 0;
+            for(int j = 1; j<=i;j++){
+                sum1 += j;
+            }
+            for(int k = i;k<=n;k++){
+                sum2 += k;
+            }
+            if(sum1 == sum2) return i;
+        }
+        return -1;
+    }
+}
