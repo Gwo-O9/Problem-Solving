6d03e3741ac49f99d12d03c559a7feba6bdae102
GeunuKim <101039305+Gwo-O9@users.noreply.github.com>
2023-01-13 18:39:33 +0900

    Create 1991. Find the Middle Index in Array.java

---
+++ LeetCode/Round13/1991. Find the Middle Index in Array.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int findMiddleIndex(int[] nums) {
+               int n = nums.length;
+        int[] leftsum = new int[n];
+        int[] rightsum = new int[n];
+        leftsum[0] = 0;
+        rightsum[n-1] = 0;
+        for(int i = 1;i<n;i++){
+            leftsum[i] = leftsum[i-1] + nums[i-1];
+            rightsum[n-i-1] = rightsum[n-i] + nums[n-i];
+        }
+        for(int j = 0; j<n;j++){
+            if(leftsum[j] == rightsum[j]) return j;
+        }
+        return -1;
+    }
+}
