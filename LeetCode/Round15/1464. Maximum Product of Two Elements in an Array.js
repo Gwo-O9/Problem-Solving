eb0d8a76215ee7730527ab8a096eedb667a2a3cf
GeunuKim <101039305+Gwo-O9@users.noreply.github.com>
2023-02-07 23:39:57 +0900

    Create 1464. Maximum Product of Two Elements in an Array.js

---
+++ LeetCode/Round15/1464. Maximum Product of Two Elements in an Array.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maxProduct = function(nums) {
+    var max = 0;
+    for(var i = 0; i<nums.length;i++){
+        for(var j = i+1;j<nums.length;j++){
+            max = Math.max(max, (nums[i]-1)*(nums[j]-1));
+        }
+    }
+    return max;
+};
