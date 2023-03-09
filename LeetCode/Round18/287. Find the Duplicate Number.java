f37474141327d20753ac32048b228069a8faf6f1
GeunuKim <101039305+Gwo-O9@users.noreply.github.com>
2023-03-09 15:32:56 +0900

    Create 287. Find the Duplicate Number.java

---
+++ LeetCode/Round18/287. Find the Duplicate Number.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int findDuplicate(int[] nums) {
+        HashSet<Integer> set = new HashSet<Integer>();
+        int n = nums.length;
+        for(int i = 0; i<n;i++){
+            if(set.contains(nums[i])) return nums[i];
+            else set.add(nums[i]);
+        }
+        return 0;
+    }
+}
