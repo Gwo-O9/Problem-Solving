40f3991a01f03337692bf85a92b1da12630f1aac
GeunuKim <101039305+Gwo-O9@users.noreply.github.com>
2023-03-11 21:51:40 +0900

    Create 27866.java

---
+++ Baekjoon/27866.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        String str = in.nextLine();
+        int n = in.nextInt();
+        System.out.println(str.charAt(n-1));
+    }
+}
