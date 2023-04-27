50161d1072cc629d2da5ffa9386e3e7b9a2e6486
GeunuKim <101039305+Gwo-O9@users.noreply.github.com>
2023-04-27 16:06:52 +0900

    Create 1927.java

---
+++ Baekjoon/1927.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.io.*;
+
+public class Main{
+     public static void main (String[] args) throws IOException{
+          BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+          int n = Integer.parseInt(br.readLine());
+          PriorityQueue<Integer> pq = new PriorityQueue<>();
+          StringBuilder sb = new StringBuilder();
+          for(int i  = 0; i<n;i++){
+               int a = Integer.parseInt(br.readLine());
+               if(a == 0) {
+                    if(pq.size() != 0) sb.append(pq.poll() + "\n");
+                    else sb.append("0\n");
+               }
+               else{
+                    pq.add(a);
+               }
+          }
+          System.out.println(sb.toString());
+     }
+}
