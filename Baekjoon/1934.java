0cc52985372d8c33cdfa803c5911342bafb114f2
GeunuKim <101039305+Gwo-O9@users.noreply.github.com>
2023-05-09 15:28:36 +0900

    Create 1934.java

---
+++ Baekjoon/1934.java
@@ -0,0 +1,28 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        for(int i = 0; i<n;i++){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            long ans = lcd(Long.parseLong(st.nextToken()), Long.parseLong(st.nextToken()));
+            System.out.println(ans);
+        }
+    }
+
+    private static long gcd(long a, long b){
+        long c;
+        while(b!=0){
+            c = a%b;
+            a = b;
+            b = c;
+        }
+        return a;
+    }
+
+    private static long lcd(long a, long b){
+        return a*b/gcd(a,b);
+    }
+}
