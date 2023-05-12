a948f9e11f5a999eee034030e12d193329b6a680
GeunuKim <101039305+Gwo-O9@users.noreply.github.com>
2023-05-12 15:39:27 +0900

    Create 25377.java

---
+++ Baekjoon/25377.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.io.*;
+import java.math.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int min = Integer.MAX_VALUE;
+        for(int i = 0; i<n; i++){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            if(a <= b) min = Math.min(b,min);
+        }
+        if(min == Integer.MAX_VALUE) System.out.println(-1);
+        else System.out.println(min);
+    }
+}
