ae7d7047ed31583699cfc3dfb4721a429d978bb1
GeunuKim <101039305+Gwo-O9@users.noreply.github.com>
2023-10-19 09:09:18 +0000

    Create 1931.py

---
+++ Baekjoon/1931.py
@@ -0,0 +1,12 @@
+n = int(input())
+times = [list(map(int, input().split())) for _ in range(n)]
+
+times.sort(key=lambda x: (x[1], x[0]))
+cnt = 1
+end_time = times[0][1]
+for i in range(1, n):
+    if times[i][0] >= end_time:
+        cnt += 1
+        end_time = times[i][1]
+
+print(cnt)
