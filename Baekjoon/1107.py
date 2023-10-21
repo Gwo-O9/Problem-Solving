cb4b6e299e2a765ced0838bf44a95443e5b82af8
GeunuKim <101039305+Gwo-O9@users.noreply.github.com>
2023-10-21 10:01:34 +0000

    Create 1107.py

---
+++ Baekjoon/1107.py
@@ -0,0 +1,18 @@
+target = int(input())
+n = int(input())
+error_keys = list(map(int, input().split()))
+ans = abs(target - 100)
+
+for i in range(1000001):
+    str_i = str(i)
+    use_broken = False
+
+    for j in str_i:
+        if int(j) in error_keys:
+            use_broken = True
+            break
+
+    if not use_broken:
+        ans = min(ans, abs(i - target) + len(str_i))
+
+print(ans)
